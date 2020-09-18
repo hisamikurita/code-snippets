7c2f4aef3365f1802df4cac3aca6a514c99dbc21
Hisami Kurita <47776346+hisamikurita@users.noreply.github.com>
2020-09-19 01:42:20 +0900

    Create barba-util.js

---
+++ barba-util.js
@@ -0,0 +1,37 @@
+/**
+ * barba.js用のユーティリティメソッド
+ */
+
+/**
+ * @descroption headタグ内のmetaタグの更新
+ */
+export function replaceHead(data) {
+    const head = document.head;
+    const newPageRawHead = data.next.html.match(/]*>([\s\S.]*)<\/head>/i)[0];
+    const newPageHead = document.createElement('head');
+    newPageHead.innerHTML = newPageRawHead;
+    const removeHeadTags = [
+        "meta[name='keywords']",
+        "meta[name='description']",
+        "meta[property^='og']",
+        "meta[name^='twitter']",
+        "meta[itemprop]",
+    ].join(',');
+
+    const headTags = head.querySelectorAll(removeHeadTags)
+    for (let i = 0; i < headTags.length; i++) {
+        head.removeChild(headTags[i]);
+    }
+    const newHeadTags = newPageHead.querySelectorAll(removeHeadTags)
+    for (let i = 0; i < newHeadTags.length; i++) {
+        head.appendChild(newHeadTags[i]);
+    }
+}
+
+/**
+ * @descroption headタグ内のbodyタグの更新
+ */
+export function replaceBody(data) {
+    const newPageRawHTML = data.next.html;
+    document.body.setAttribute('class', newPageRawHTML.match(/<body .*class="(.*)">/i,)[1]);
+}
